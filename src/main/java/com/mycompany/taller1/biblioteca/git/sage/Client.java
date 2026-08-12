adc0d7f56b16374ffc3f2b6b8454581f43380c01
sergioaguerrero <sergioaguerrero@unicesar.edu.co>
2026-08-12 11:15:36 -0500

    feat: crear clase Cliente heredando de Persona

---
+++ src/main/java/com/mycompany/taller1/biblioteca/git/sage/Client.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller1.biblioteca.git.sage;
+
+/**
+ *
+ * @author ESTUDIANTES
+ */
+public class Client {
+    
+}
