915de4c5d4b8693cba69c2f44cf5b7c06f39e493
sergioaguerrero <sergioaguerrero@unicesar.edu.co>
2026-08-12 10:53:43 -0500

    feat: inicializar proyecto Maven de biblioteca

---
+++ src/main/java/com/mycompany/taller1/biblioteca/git/sage/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller1.biblioteca.git.sage;
+
+/**
+ *
+ * @author ESTUDIANTES
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
